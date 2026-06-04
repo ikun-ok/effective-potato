dcc0dbe5be63e3dbaafd81c7a971acd954802380
ikun-ok <2506315950@qq.com>
2026-06-04 15:10:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import base64
 import requests
-import streamlit as st
 
-def get_qwen_key():
-    return st.secrets["QWEN_KEY"]
+QWEN_KEY = "sk-9e13e1949f1f4ce1a957837ac0c539bc"
 
-# 菜品识图
 def analyze_dish(img_base64):
-    api_key = get_qwen_key()
+    api_key = QWEN_KEY
     url = "https://dashscope.aliyuncs.com/api/v1/services/aigc/multimodal-generation/generation"
     headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
     prompt = """
@@ -17,7 +14,7 @@ def analyze_dish(img_base64):
 热量：数字 kcal
 """
     data = {
-        "model": "qwen-vl-plus",
+        "model": "qwen-plus-latest",
         "messages": [
             {
                 "role": "user",
@@ -30,7 +27,6 @@ def analyze_dish(img_base64):
     }
     res = requests.post(url, headers=headers, json=data)
     json_data = res.json()
-    # 兼容返回结构
     result_text = json_data["output"]["choices"][0]["message"]["content"][0]["text"]
     return result_text
 
@@ -41,24 +37,14 @@ def food_agent_run(img_file):
     dish_name = res_str.split("菜名：")[1].split("\n")[0].strip()
     material = res_str.split("食材：")[1].split("\n")[0].strip()
     cal_num = res_str.split("热量：")[1].replace("kcal", "").strip()
-    return {
-        "name": dish_name,
-        "material": material,
-        "cal": cal_num
-    }
+    return {"name": dish_name,"material": material,"cal": cal_num}
 
-# 健康问答
 def health_chat_answer(question):
-    api_key = get_qwen_key()
+    api_key = QWEN_KEY
     url = "https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation"
     headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
-    prompt = f"""你是私人健康营养师，简短回答饮食、运动、睡眠、减脂相关问题。
-用户提问：{question}
-"""
-    data = {
-        "model": "qwen-turbo",
-        "messages": [{"role": "user", "content": prompt}]
-    }
+    prompt = f"""你是私人健康营养师，简短回答饮食、运动、睡眠、减脂相关问题。用户：{question}"""
+    data = {"model": "qwen3.7-plus","messages": [{"role": "user", "content": prompt}]}
     res = requests.post(url, headers=headers, json=data)
     json_data = res.json()
     return json_data["output"]["text"]
